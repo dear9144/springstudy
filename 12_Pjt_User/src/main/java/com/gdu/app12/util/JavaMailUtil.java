8862bd761b7d960961b72b8ef0c5ca070b9999fc
dear9144 <dear9144@naver.com>
2023-05-15 11:36:10 +0900

    33

--- 12_Pjt_User/src/main/java/com/gdu/app12/util/JavaMailUtil.java
+++ 12_Pjt_User/src/main/java/com/gdu/app12/util/JavaMailUtil.java
@@ -1,5 +1,57 @@
 package com.gdu.app12.util;
 
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@PropertySource(value={"classpath:application.properties"})
+@Component
 public class JavaMailUtil {
 
+	@Autowired
+	private Environment env;
+	
+	public void sendJavaMail(String to, String title, String content) {
+		try {
+			
+			Properties properties = new Properties();
+			properties.put("mail.smtp.host", env.getProperty("spring.mail.host"));
+			properties.put("mail.smtp.port", env.getProperty("spring.mail.post"));
+			properties.put("mail.smtp.auth", env.getProperty("spring.mail.properties.mail.smtp.auth"));
+			properties.put("mail.smtp.starttls.enable", env.getProperty("spring.mail.properties.mail.smtp.mail.smtp.starttls.enable"));
+			// 이메일 보내는 계정 정보를 javax.mail.Session에 저장한다 
+			MimeMessage message = new MimeMessage(Session.getInstance(properties, new Authenticator() {
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(env.getProperty("spring.mail.username"), env.getProperty("spring.mail.password"));
+				}
+			
+			}));
+			//이메일 만들기
+			message.setFrom(new PasswordAuthentication(env.getProperty("spring.mail.username"), "사이트관리자"));
+			message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			message.setSubject(title);
+			message.setContent(content, "text/html; charset=UTF-8");
+			
+			//이메일 보내기
+			Transport.send(message);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
 }
